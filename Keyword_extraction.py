e39c50fa91ed2210ecccbe484df3e4273ca920a2
Simarjot Singh (Monga) <simar5244@gmail.com>
2022-10-05 22:12:34 +0530

    Update Keyword_extraction.py

--- Keyword_extraction.py
+++ Keyword_extraction.py
@@ -1,6 +1,6 @@
 from rake_nltk import Rake
 rake_nltk_var = Rake()
-text = """ {input} """
+text = """ Hello, my name is Simarjot """
 rake_nltk_var.extract_keywords_from_text(text)
 keyword_extracted = rake_nltk_var.get_ranked_phrases()
 print(keyword_extracted)
